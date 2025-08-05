8e610a3744eed818ea8b0b2f96fc452122b57cef
zukkari <stanislav.myshkovski@gmail.com>
2025-08-05 21:54:17 +0300

    change router type

---
+++ app/entry.client.tsx
@@ -0,0 +1,24 @@
+﻿import {startTransition, StrictMode} from "react";
+import {hydrateRoot} from "react-dom/client";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import Card from "~/routes/card";
+
+startTransition(() => {
+    const router = createBrowserRouter(
+        [
+            {
+                path: "/",
+                Component: Card
+            }
+        ],
+        {
+            basename: "/wcard"
+        });
+
+    hydrateRoot(
+        document,
+        <StrictMode>
+            <RouterProvider router={router}/>
+        </StrictMode>,
+    );
+});
